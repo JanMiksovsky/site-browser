d23b0ea636bcaba54d56fa3749534fa317f32926
Jan Miksovsky <jan@miksovsky.com>
2024-09-18 22:05:23 -0700

    Styling

--- src/columns.js
+++ src/columns.js
@@ -1,7 +1,4 @@
-import {
-  SiteTree,
-  Tree,
-} from "./node_modules/@weborigami/async-tree/browser.js";
+import SiteTree from "./node_modules/@weborigami/async-tree/src/SiteTree.js";
 
 const location = document.location;
 const url = location.search?.slice(1);
@@ -13,7 +10,7 @@ if (url) {
   const tree = new SiteTree(url);
   const keys = await tree.keys();
   const promises = keys.map(async (key) => {
-    const isSubtree = await Tree.isKeyForSubtree(tree, key);
+    const isSubtree = await tree.isKeyForSubtree(key);
     const href = isSubtree ? `index.html?${url}/${key}` : `${url}/${key}`;
     const classAttr = isSubtree ? `class="subtree"` : "";
     return `<a href="${href}" ${classAttr}>${key}</a>`;
